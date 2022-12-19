0ecc587ae6c4ca7af4fdc13828f4f971635461ec
PO-WEI, CHEN <56902242+bobo0303@users.noreply.github.com>
2022-12-19 17:46:42 +0800

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,10 @@
+# AICUP-plant-Autumn (TEAM_2042)
+組員: wiwi6166, julian135707, louis52099, 97a4a07, GoofyAhh
+Only for AI CUP group and private notes
 
+---
+## 農地作物現況調查影像辨識競賽 - 春季賽：AI作物影像判釋
+![image](https://i.imgur.com/TXrb6aw.jpeg)
+---
+
+## 文件結構說明
